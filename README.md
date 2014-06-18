9a987f78567e1bc76193099b34ab83f68961a28f
Sergej Müller <s.mueller@superreal.de>
2014-06-18 20:39:18 +0200

    Version 0.0.1

---
+++ README.md
@@ -0,0 +1,45 @@
+## Publish Confirm
+
+
+Zu oft WordPress-Artikel versehentlich publiziert statt sie als Entwurf zu speichern? Das WordPress-Plugin „Publish Confirm“ schaltet eine zusätzliche Abfrage zwischen Klick und Funktionsweise der Schaltfläche _Veröffentlichen_. Simpel und effektiv.
+
+Sobald ein Artikel veröffentlicht wurde, wird für diesen Artikel keine Zwischenabfrage (mehr) geschaltet.
+
+
+#### Mindestvoraussetzungen
+
+* WordPress 3.9
+* PHP 5.2.4
+
+
+#### Inbetriebnahme
+
+1. ZIP herunterladen
+2. Plugin in WordPress installieren
+    1. Wahlweise den entpackten Ordner per SFTP übertragen
+    2. Oder die ZIP-Datei direkt in WordPress hochladen
+3. Plugin aktivieren
+
+
+#### Versionsverlauf
+
+* 0.0.1 Init-Version
+
+
+##### Autor
+
+[Sergej Müller](http://wpcoder.de)
+
+
+##### Donuts
+
+* [Gittip](https://www.gittip.com/sergejmueller/)
+* [Flattr](https://flattr.com/submit/auto?user_id=sergej.mueller&url=https%3A%2F%2Fgithub.com%2Fsergejmueller%2FPublish-Confirm)
+* [PayPal](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=5RDDW9FEHGLG6)
+
+
+##### Profil
+
+* [Google+](https://plus.google.com/110569673423509816572?rel=author)
+* [Twitter](https://twitter.com/wpSEO)
+* [Impressum](http://wpcoder.de)
---
+++ publish-confirm.php
@@ -0,0 +1,62 @@
+<?php
+/*
+Plugin Name: Publish confirm
+Description: Zusatzabfrage in WordPress beim Klick auf den Button "Veröffentlichen"
+Author: Sergej M&uuml;ller
+Author URI: http://wpcoder.de
+Plugin URI:
+License: GPLv2 or later
+Version: 0.0.1
+*/
+
+/*
+Copyright (C)  2014-2014 Sergej Müller
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+
+/* Quit */
+defined('ABSPATH') OR exit;
+
+
+/* Action! */
+add_action(
+    'admin_footer-post-new.php',
+    'publish_confirm_embed'
+);
+
+
+/* Embed plugin js */
+function publish_confirm_embed() {
+    if ( ! wp_script_is('jquery', 'done') ) {
+        return;
+    } ?>
+
+    <script>
+        jQuery(document).ready(
+            function($){
+                $('#publish').on(
+                    'click',
+                    function(event) {
+                        if ( ! confirm('Wirklich veröffentlichen?') ) {
+                            event.preventDefault();
+                        }
+                    }
+                );
+            }
+        );
+    </script>
+<?php }
